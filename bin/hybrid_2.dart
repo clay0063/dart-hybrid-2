ed06b3bc3fa08772e0149aaee0c86b4c1a7aa152
Teagan Clayton <clay0063@algonquinlive.com>
2023-09-25 12:15:39 -0400

    Basic error checking

--- bin/hybrid_2.dart
+++ bin/hybrid_2.dart
@@ -1,10 +1,12 @@
-import 'dart:io';
 import 'package:hybrid_2/hybrid_2.dart' as hybrid_2;
 
 void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print("No names provided!");
+  } else {
   for (String arg in arguments){
     print(arg);
-  }
+  } }
 
   final randomNumber = hybrid_2.Lucky();
   print('The random number is ${randomNumber.randomNumber}');
--- lib/hybrid_2.dart
+++ lib/hybrid_2.dart
@@ -2,8 +2,8 @@ import 'dart:math';
 import 'dart:io';
 
 class Lucky {
-  final int min = int.tryParse(Platform.environment['MIN'] ?? '0') ?? 0;
-  final int max = int.tryParse(Platform.environment['MAX'] ?? '10') ?? 10;
+  int min = int.tryParse(Platform.environment['MIN'] ?? '0') ?? 0;
+  int max = int.tryParse(Platform.environment['MAX'] ?? '10') ?? 10;
   late int randomNumber;
 
   Lucky(){
