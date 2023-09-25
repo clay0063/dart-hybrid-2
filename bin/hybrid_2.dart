ac109e4d77814a1e377a3c2660662070af492dad
Teagan Clayton <clay0063@algonquinlive.com>
2023-09-25 12:07:21 -0400

    Add ability to read args

--- bin/hybrid_2.dart
+++ bin/hybrid_2.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
 import 'package:hybrid_2/hybrid_2.dart' as hybrid_2;
 
 void main(List<String> arguments) {
+  for (String arg in arguments){
+    print(arg);
+  }
+
   final randomNumber = hybrid_2.Lucky();
   print('The random number is ${randomNumber.randomNumber}');
 }
--- lib/hybrid_2.dart
+++ lib/hybrid_2.dart
@@ -3,11 +3,10 @@ import 'dart:io';
 
 class Lucky {
   final int min = int.tryParse(Platform.environment['MIN'] ?? '0') ?? 0;
-  final int max = int.tryParse(Platform.environment['MAX'] ?? '10') ?? 0;
-  int randomNumber = 0;
+  final int max = int.tryParse(Platform.environment['MAX'] ?? '10') ?? 10;
+  late int randomNumber;
 
   Lucky(){
-
     final random = Random();
     randomNumber = min + random.nextInt(max - min + 1);
   }
