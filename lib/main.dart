e099069f56ebd4cf64c5f4ce6a687cc858a6beed
Teagan Clayton <clay0063@algonquinlive.com>
2023-09-28 22:25:32 -0400

    Move error handling to constructor

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,20 @@ import 'dart:math';
 import 'dart:io';
 
 class Lucky {
-  int min = int.tryParse(Platform.environment['MIN'] ?? '0') ?? 0; 
+  int min = int.tryParse(Platform.environment['MIN'] ?? '0') ?? 0;
   int max = int.tryParse(Platform.environment['MAX'] ?? '10') ?? 10;
   //takes these assigned variables from the terminal environment, but if they don't exist, assign them to 0 - 10
 
-  Lucky();
-
-  int randomNumber(){
+  Lucky() {
     if (min > max) {
       int temp = min;
       min = max;
       max = temp;
     }
+  }
+
+  int randomNumber() {
     int range = max - min;
     return Random().nextInt(range) + min;
   }
-
-
 }
